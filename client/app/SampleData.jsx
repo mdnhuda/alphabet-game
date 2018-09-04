57d08eee97d297536ede0ace83e1fb6ea0b1c41f
Naimul Huda <mdnhuda@gmail.com>
2018-09-05 00:14:28 +1000

    Added axios, reading sample data from backend

--- client/app/SampleData.jsx
+++ client/app/SampleData.jsx
@@ -1,19 +1,19 @@
 const SampleData = {
+    //alphabet: {
+    //    label: "B",
+    //    value: "b",
+    //    image: "http://pngimg.com/uploads/football/football_PNG52781.png",
+    //    cursive: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/B_cursiva.gif/400px-B_cursiva.gif",
+    //    audio: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Voiced_bilabial_plosive.ogg",
+    //    audioType: "audio/ogg"
+    //},
+    //keyboard: [{value: "a", label: "A"}, {value: "b", label: "B"}, {value: "c", label: "C"}],
     title: "The Alphabet Game",
-    alphabet: {
-        label: "B",
-        value: "b",
-        image: "http://pngimg.com/uploads/football/football_PNG52781.png",
-        cursive: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/B_cursiva.gif/400px-B_cursiva.gif",
-        audio: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Voiced_bilabial_plosive.ogg",
-        audioType: "audio/ogg"
-    },
     settings: {
-        language: "en",
+        language: "EN",
         audioAutoPlay: false,
         audioLoop: false
-    },
-    keyboard: [{value: "a", label: "A"}, {value: "b", label: "B"}, {value: "c", label: "C"}]
+    }
 };
 
 export default SampleData;
