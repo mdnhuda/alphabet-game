1f974dcf45e53c4b7a4a8bd5ab5cdc7fed4fd361
nhuda <nhuda@viator.com>
2018-09-28 23:54:52 +1000

    Fixed maven build

--- services/src/main/java/com/mdnhuda/alphabetGame/service/AlphabetService.java
+++ services/src/main/java/com/mdnhuda/alphabetGame/service/AlphabetService.java
@@ -1,17 +1,22 @@
 package com.mdnhuda.alphabetGame.service;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mdnhuda.alphabetGame.domain.*;
+import com.mdnhuda.alphabetGame.domain.Alphabet;
+import com.mdnhuda.alphabetGame.domain.AlphabetType;
+import com.mdnhuda.alphabetGame.domain.KeyboardEntry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
-
-import java.io.IOException;
-import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * @author mdnhuda@gmail.com
@@ -29,14 +34,16 @@ public class AlphabetService {
     private Map<AlphabetType, List<Alphabet>> alphabetMap;
     private Map<AlphabetType, List<KeyboardEntry>> keyboardMap;
 
-    public AlphabetService() throws IOException {
+    @Autowired
+    public AlphabetService(ApplicationContext context) throws IOException {
         Map<AlphabetType, List<Alphabet>> map = new HashMap<>();
         TypeReference<List<Alphabet>> alphabetListType = new TypeReference<List<Alphabet>>() {
         };
 
-        map.put(AlphabetType.EN_UPPER, MAPPER.readValue(ResourceUtils.getFile("classpath:english_upper.json"), alphabetListType));
+        map.put(AlphabetType.EN_UPPER,
+                MAPPER.readValue(context.getResource("classpath:english_upper.json").getInputStream(), alphabetListType));
 
-        map.put(AlphabetType.BN, MAPPER.readValue(ResourceUtils.getFile("classpath:bangla.json"), alphabetListType));
+        map.put(AlphabetType.BN, MAPPER.readValue(context.getResource("classpath:bangla.json").getInputStream(), alphabetListType));
 
         this.alphabetMap = map;
 
@@ -50,7 +57,7 @@ public class AlphabetService {
     }
 
     public Alphabet getAlphabet(AlphabetType alphabetType, int id) {
-        return alphabetMap.get(alphabetType).stream().filter(alphabet -> alphabet.getId() == id).findFirst().get();
+        return alphabetMap.get(alphabetType).stream().filter(alphabet -> alphabet.getId() == id).findFirst().orElse(null);
     }
 
     public List<KeyboardEntry> getKeyboard(AlphabetType alphabetType) {
--- services/src/test/java/com/mdnhuda/alphabetGame/service/AlphabetServiceTest.java
+++ services/src/test/java/com/mdnhuda/alphabetGame/service/AlphabetServiceTest.java
@@ -1,22 +1,33 @@
 package com.mdnhuda.alphabetGame.service;
 
+import com.mdnhuda.alphabetGame.WebMvcConfig;
 import com.mdnhuda.alphabetGame.domain.Alphabet;
 import com.mdnhuda.alphabetGame.domain.AlphabetType;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by naimulhuda on 26/09/2018.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextHierarchy(@ContextConfiguration(classes = WebMvcConfig.class))
 public class AlphabetServiceTest {
     private AlphabetService service;
 
+    @Autowired
+    private ApplicationContext context;
+
     @Before
     public void setUp() throws Exception {
-        service = new AlphabetService();
+        service = new AlphabetService(context);
     }
 
     @Test
